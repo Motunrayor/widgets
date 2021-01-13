0a04887d33bb7cda271e4fc441caae124e674bf0
motunrayo <akinrimisimotunrayo@gmail.com>
2021-01-13 12:16:46 +0100

    search component

--- src/components/Accordion/search/search.js
+++ src/components/Accordion/search/search.js
@@ -2,48 +2,48 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  // const [text, setText] = useState("programming");
-  // const [results, setResults] = useState([]);
+  const [text, setText] = useState("programming");
+  const [results, setResults] = useState([]);
 
-  // console.log(results);
+  console.log(results);
 
-  // useEffect(() => {
-  //   const search = async () => {
-  //     const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-  //       params: {
-  //         action: "query",
-  //         list: "search",
-  //         origin: "*",
-  //         format: "json",
-  //         srsearch: text,
-  //       },
-  //     });
-  //     setResults(data.query.search);
-  //   };
-  //   if (text) {
-  //     search();
-  //   } else {
-  //     const timeOutId = setTimeout(() => {
-  //       if (text) {
-  //         search();
-  //       }
-  //     }, 1000);
-  //     return () => {
-  //       clearTimeout(timeOutId);
-  //     };
-  //   }
-  // }, [text]);
-  // const onInputChange = (e) => {
-  //   setText(e.target.value);
-  // };
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: text,
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (text) {
+      search();
+    } else {
+      const timeOutId = setTimeout(() => {
+        if (text) {
+          search();
+        }
+      }, 1000);
+      return () => {
+        clearTimeout(timeOutId);
+      };
+    }
+  }, [text]);
+  const onInputChange = (e) => {
+    setText(e.target.value);
+  };
 
   return (
     <div>
       <form>
         <title>Search</title>
-        {/* <input type="text" value={text} onChange={(e) => onInputChange(e)} /> */}
+        <input type="text" value={text} onChange={(e) => onInputChange(e)} />
       </form>
-      {/* {results.map((result) => {
+      {results.map((result) => {
         return (
           <div>
             <div key={result.pageid}>
@@ -56,7 +56,7 @@ const Search = () => {
             </div>
           </div>
         );
-      })} */}
+      })}
     </div>
   );
 };
